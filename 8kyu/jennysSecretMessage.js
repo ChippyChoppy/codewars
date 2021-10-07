fbfe9527119049c1fe364a1806cc6912e2843bfa
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-10-07 14:04:49 -0400

    fixing errors

---
+++ 8kyu/jennysSecretMessage.js
@@ -0,0 +1,25 @@
+/**
+ * Jenny has written a function that returns a greeting for a user.
+ * However, she's in love with Johnny, and would like to greet him
+ * slightly different.
+ * She added a special case to her function, but she made a mistake.
+ * 
+ * Can you help her?
+ */
+
+/*** Original Function ***/
+
+function greet(name) {
+    return "Hello, " + name + "!";
+    if (name === "Johnny") 
+        return "Hello, my love!";
+}
+
+/*** Fixed Function ***/
+
+function greetFixed(name) {
+    if (name === "Johnny") return "Hello, my love!"
+    else return "Hello, " + name + "!"
+}
+
+console.log(greetFixed("Becca"))
